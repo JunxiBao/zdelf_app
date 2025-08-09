c566b9760502ff2612c40cfd575735e7e1c10d3b
JunxiBao <baojunxilogin@icloud.com>
2025-08-09 19:36:49 +0800

    2

--- src/backend/app.py
+++ src/backend/app.py
@@ -3,12 +3,14 @@ from flask_cors import CORS
 from routes.login import login_blueprint
 from routes.register import register_blueprint
 from routes.readdata import readdata_blueprint
+from routes.editdata import editdata_blueprint
 from routes.deepseek import deepseek_blueprint
 
 app = Flask(__name__)
 app.register_blueprint(login_blueprint)
 app.register_blueprint(register_blueprint)
 app.register_blueprint(readdata_blueprint)
+app.register_blueprint(editdata_blueprint)
 app.register_blueprint(deepseek_blueprint, url_prefix='/deepseek')
 CORS(app, resources={r"/*": {"origins": "https://zhucan.xyz"}}, supports_credentials=True)
 
